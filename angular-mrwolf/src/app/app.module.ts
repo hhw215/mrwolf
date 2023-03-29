30141d915a84e34021dc3e55057ce47ec5a5f790
华伟韩 <huawei@s-MacBook-Pro.broadband>
2023-03-29 11:08:47 +0200

    angular frontend login and welcome pages finish

--- angular-mrwolf/src/app/app.module.ts
+++ angular-mrwolf/src/app/app.module.ts
@@ -6,6 +6,17 @@ import { CustomersComponent } from './components/customers/customers.component';
 import { LoginComponent } from './components/login/login.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 
+// angular material imports in app.module.ts
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// // for compiling bug : @NgModule imports AppRoutingModule
+// import { RouterModule, Routes } from '@angular/router';
+
+//Add the imported modules to the imports array in the @NgModule decorator line 24-32
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +25,13 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
     WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+//     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
